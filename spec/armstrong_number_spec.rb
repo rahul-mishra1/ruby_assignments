63ebecd491b2c28efc1a5993405f84dfae354eb9
Dibyajyoti Dhar <dibyajyoti.dhar@anchanto.com>
2021-02-10 21:30:41 +0530

    some clarifications

--- spec/armstrong_number_spec.rb
+++ spec/armstrong_number_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'ArmstrongNumber' do
     end
   end
 
-  it 'should return false if no is not armstrong - wrong test case' do
+  it 'should return false if no is not armstrong - wrong test case [will be flashed in red as 0 is an armstrong number]' do
     [0,10,12,14,15].each do |num|
       expected = ArmstrongNumber.new(num).is_armstrong
       expect(expected).to eq(false)
@@ -20,6 +20,10 @@ RSpec.describe 'ArmstrongNumber' do
       expected = ArmstrongNumber.new(num).is_armstrong
       expect(expected).to eq(false)
     end
+  end
+
+  it 'zero is an armstrong number' do
+    expect(ArmstrongNumber.new(0).is_armstrong).to eq(true)
   end
 
 end
